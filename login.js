31390929440398bc32fde0772780583e946e20da
reethuthota <reethu.thota@gmail.com>
2024-01-14 07:06:14 +0530

    add code

---
+++ login.js
@@ -0,0 +1,4 @@
+function redirectRecruiterLogin() {
+    // Redirect to the recruiter login page (replace 'recruiter-login.html' with your desired page)
+    window.location.href = 'https://auth-supabase-ten.vercel.app/';
+}
